dc9297c9b5732b9e5fae4e714c74d1fdf0fd972d
xuri <T0Bl4S.durst@gmail.com>
2019-01-20 19:43:33 +0100

    finisghed

--- Patch/Form1.cs
+++ Patch/Form1.cs
@@ -22,52 +22,17 @@ namespace Patch
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var pat = Path.GetDirectoryName(Application.ExecutablePath) + @"\tmp\";
-
-            Directory.CreateDirectory(pat);
-
-            File.Create(pat + "Application Manager.exe").Close();
-            File.Create(pat + "Creator.exe").Close();
-            File.Create(pat + "Installer.exe").Close();
-            File.Create(pat + "AIO.dll").Close();
-
-            File.WriteAllBytes(pat + "Application Manager.exe", Strings.build(Strings.Application_Manager_exe));
-            File.WriteAllBytes(pat + "Creator.exe", Strings.build(Strings.Creator_exe));
-            File.WriteAllBytes(pat + "Installer.exe", Strings.build(Strings.Installer_exe));
-            File.WriteAllBytes(pat + "AIO.dll", Strings.build(Strings.AIO_dll));
-
-            Process.Start(pat + "Application Manager.exe");
-            Thread.Sleep(3000);
-            Process.Start("taskkill", "/IM \"Application Manager.exe\" /f");
-            Process.Start(pat + "Creator.exe");
-            Thread.Sleep(3000);
-            Process.Start("taskkill", "/IM Creator.exe /f");
-            Process.Start(pat + "Installer.exe");
-            Thread.Sleep(6000);
-            Process.Start("taskkill", "/IM Installer.exe /f");
-            Thread.Sleep(1000);
-            Process.Start(@"C:\Windows" + @"\Installer_Custom\" + "Installer.exe");
-            Thread.Sleep(2000);
-            Process.Start("taskkill", "/IM Installer.exe /f");
-
-            fas.FileAssociation.CreateAssociation("_Installer_custom_", "Installer", "C:\\Windows\\Installer_Custom\\Installer.exe", "inst", "C:\\Windows\\Installer_Custom\\Installer.ico");
+            Program.Install();
         }
-
         private void button2_Click(object sender, EventArgs e)
         {
-            button3_Click(null, null);
-            button1_Click(null, null);
+            Program.UnInstall();
+            Program.Install();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            try
-            {
-                Directory.Delete(@"C:\Windows" + @"\Installer_Custom\", true);
-            }
-            catch { }
-            fas.FileAssociation.DeleteAssociation("_Installer_custom_", "inst");
-            MessageBox.Show("Uninstall surccess!");
+            Program.UnInstall();
         }
     }
 }
--- Patch/Program.cs
+++ Patch/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -17,6 +20,51 @@ namespace Patch
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+
+        public static void Install()
+        {
+            var pat = Path.GetDirectoryName(Application.ExecutablePath) + @"\tmp\";
+
+            Directory.CreateDirectory(pat);
+
+            File.Create(pat + "Application Manager.exe").Close();
+            File.Create(pat + "Creator.exe").Close();
+            File.Create(pat + "Installer.exe").Close();
+            File.Create(pat + "AIO.dll").Close();
+
+            File.WriteAllBytes(pat + "Application Manager.exe", Packer.GET_Strings.build(Packer.GET_Strings.Application_Manager_exe));
+            File.WriteAllBytes(pat + "Creator.exe", Packer.GET_Strings.build(Packer.GET_Strings.Creator_exe));
+            File.WriteAllBytes(pat + "Installer.exe", Packer.GET_Strings.build(Packer.GET_Strings.Installer_exe));
+            File.WriteAllBytes(pat + "AIO.dll", Packer.GET_Strings.build(Packer.GET_Strings.AIO_dll));
+
+            Process.Start(pat + "Application Manager.exe");
+            Thread.Sleep(3000);
+            Process.Start("taskkill", "/IM \"Application Manager.exe\" /f");
+            Process.Start(pat + "Creator.exe");
+            Thread.Sleep(3000);
+            Process.Start("taskkill", "/IM Creator.exe /f");
+            Process.Start(pat + "Installer.exe");
+            Thread.Sleep(6000);
+            Process.Start("taskkill", "/IM Installer.exe /f");
+            Thread.Sleep(1000);
+            Process.Start(@"C:\Windows" + @"\Installer_Custom\" + "Installer.exe");
+            Thread.Sleep(2000);
+            Process.Start("taskkill", "/IM Installer.exe /f");
+
+            fas.FileAssociation.CreateAssociation("_Installer_custom_", "Installer", "C:\\Windows\\Installer_Custom\\Installer.exe", "inst", "C:\\Windows\\Installer_Custom\\Installer.ico");
+            MessageBox.Show("Instalation erfolgreich!");
+        }
+
+        internal static void UnInstall()
+        {
+            try
+            {
+                Directory.Delete(@"C:\Windows" + @"\Installer_Custom\", true);
+            }
+            catch { }
+            fas.FileAssociation.DeleteAssociation("_Installer_custom_", "inst");
+            MessageBox.Show("Uninstall surccess!");
         }
     }
 }
